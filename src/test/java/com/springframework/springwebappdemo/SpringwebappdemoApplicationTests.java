7043a2cfe783b97d6561c4e15a5da0a5ad4471d2
Greg <gregmatthews1994@gmail.com>
2021-10-27 08:39:34 -0400

    initial push

---
+++ src/test/java/com/springframework/springwebappdemo/SpringwebappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.springwebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
